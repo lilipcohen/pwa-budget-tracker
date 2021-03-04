6f5af9a94da8230bef6fd520535191b1319d25ed
Lili Cohen <lili.cohen.tv@gmail.com>
2021-03-03 17:01:13 -0800

    fixing bugs

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,13 @@
 const FILES_TO_CACHE = [
   "/",
-  "/index.html",
+  // "/index.html",
   "/styles.css",
   "/index.js",
   "/db.js",
-  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
-  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
-  "/manifest.webmanifest",
-  "/service-worker.js",
+  // "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+  // "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+  // "/manifest.webmanifest",
+  // "/service-worker.js",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png"
   
